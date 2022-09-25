3896c924005593f36be14c82a554ef0a8abc5b5a
faspiru <fabrizio.spiotta@correo.unimet.edu.ve>
2022-09-25 19:47:41 -0400

    :sparkles:

--- Ejercicio31.py
+++ Ejercicio31.py
@@ -1,15 +1,15 @@
 tabla_desde = input("Tabla de multiplicar del: ")
-while not tabla_desde.isnumeric or not int(tabla_desde) >= 0:
-    print("Ingreso Invalido, Tabla de multiplicar desde: ")
+while not tabla_desde.isnumeric() or not int(tabla_desde) >= 0:
+    tabla_desde = input("Ingreso Invalido, Tabla de multiplicar desde: ")
 tabla_hasta = input("Tabla de multiplicar hasta: ")
-while not tabla_hasta.isnumeric or not int(tabla_hasta) >= 0:
-    print("Ingreso Invalido, Tabla de multiplicar hasta: ")
+while not tabla_hasta.isnumeric() or not int(tabla_hasta) > int(tabla_desde):
+    tabla_hasta = input("Ingreso Invalido, Tabla de multiplicar hasta: ")
 desde = input("Desde el: ")
-while not desde.isnumeric or not int(desde) >= 0:
-    print("Ingreso Invalido, Desde el: ")
+while not desde.isnumeric() or not int(desde) >= 0:
+    desde = input("Ingreso Invalido, Desde el: ")
 hasta = input("Hasta el: ")
-while not hasta.isnumeric or not int(hasta) >= 0:
-    print("Ingreso Invalido, Hasta el: ")
+while not hasta.isnumeric() or not int(hasta) > int(desde):
+    hasta = input("Ingreso Invalido, Hasta el: ")
 
 tabla_desde = int(tabla_desde)
 tabla_hasta = int(tabla_hasta)
